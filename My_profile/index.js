a710d690497307509e528e54ba1ccd9ffc2a166e
praveen kumar <84777375+praveenfearless08@users.noreply.github.com>
2022-09-02 18:50:44 +0530

    Add files via upload

---
+++ My_profile/index.js
@@ -0,0 +1,11 @@
+$("img.cloud3").on("mouseover",function(){
+    $("img.cloud3").animate({opacity:0.8},"slow");
+});
+
+$("h2.hello").on("click",function(){
+    $("h2.hello").text("Welcome");
+});
+
+// $(".cloud1").fadeIn(100).fadeOut(100).fadeIn(100);
+// $(".cloud2").fadeIn(100).fadeOut(100).fadeIn(100);
+// $(".cv").fadeIn(100).fadeOut(100).fadeIn(100);
